acc845c58d90b6cd652a8de3a62ce25e9ace5873
Dhairya <dhairyasaraswat29@gmail.com>
2022-11-14 12:29:55 +0530

    UploadData

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { ServiceService } from './service.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'QuizTask';
+  newdata: any;
+  constructor(private _apiservice: ServiceService) { }
+ 
+  saveCont: any = [];
+  value: any;
+  countryData:any;
+  score=0;
+  ngOnInit() {
+    this._apiservice.getdata().subscribe((res: any) => {
+      this.newdata = res.data;
+
+      for (let i = 0; i < 5; i++) {
+        let countryNum = Math.floor(Math.random() * this.newdata.length);
+
+        this.saveCont.push(this.newdata[countryNum]);
+      }
+      console.log(this.saveCont);
+      console.log(res.data);
+    })
+  }
+
+  check(value:any,countryData:any,index:any){
+    console.log(value);
+    console.log(index);
+    if (value==countryData.capital)
+    {
+      //alert("correct")
+      this.score=this.score+1;
+    }
+    this.saveCont[index]['Answered'] = value;
+
+    
+    // console.log(countryData);
+    
+    console.log(this.saveCont);
+    
+}
+res(){
+  alert(this.score)
+  var x:any;
+  x = document.getElementById("showdata");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
 }
